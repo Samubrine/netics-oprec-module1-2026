fedbf1edc118bf6360a5bf83b20d5929c63916ef
Sam <ibra.mikail1@gmail.com>
2026-03-27 17:52:38 +0700

    init

---
+++ src/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from datetime import datetime
+import time
+
+app = FastAPI()
+start_time = time.time()
+
+@app.get("/health")
+async def health():
+    return {
+        "nama": "Mikail Ibrahim Hakim",
+        "nrp": "5025241046",
+        "status": "UP",
+        "timestamp": datetime.now().isoformat(),
+        "uptime": time.time() - start_time
+    }
